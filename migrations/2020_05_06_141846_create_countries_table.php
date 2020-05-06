702b156690450f50fa8db56fddca2c23633d8a3a
p0ep0e <me@bobmckay.com>
2020-05-06 15:48:25 +0100

    Initial Commit

---
+++ migrations/2020_05_06_141846_create_countries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->smallInteger('id')->unsigned()->autoIncrement();
+            $table->string('name_english');
+            $table->char('ISO_2_letter',2);
+            $table->char('ISO_3_letter',3);
+            $table->char('TLD',3);
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
